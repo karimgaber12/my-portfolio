18fee216c0b6ef785249865211a1ecd39d305955
karim gaber <karimgaberhd@gmail.com>
2022-02-06 13:44:53 +0200

    'updated'

--- JS/main.js
+++ JS/main.js
@@ -29,6 +29,7 @@ let toggle1 = document.querySelector(".toogle1")
 let toggle2 = document.querySelector(".toogle2")
 
 let nav = document.querySelector(".navbar")
+
 toggler.addEventListener('click',()=>{
 
     toggle1.classList.toggle("active1");
@@ -36,6 +37,21 @@ toggler.addEventListener('click',()=>{
     nav.classList.toggle("active")
 })
 
+/*
+const body = document.querySelector(".main")
+
+body.addEventListener('click',() => {
+
+    if (body.classList.contains("main")) {
+
+        nav.classList.toggle("active")
+        toggle1.classList.toggle("active1");
+        toggle2.classList.toggle("active");
+    
+    }
+    
+})
+*/
 // close nav when i clicked to section
 
 function togglemenu() {
@@ -56,27 +72,27 @@ timeLineIntro.fromTo('.social_media',{y:200,opacity:0},{y:0,opacity:1},'<')
 
 //animation aboutMe
 let aboutSection = document.getElementById("About-me")
-const timeLineAbout = gsap.timeline({scrollTrigger:"#About-me",defaults:{duration:1.5,ease: Power1.easeInOut}})   
+const timeLineAbout = gsap.timeline({scrollTrigger:"#About-me",defaults:{duration:0.8,ease: Power1.easeInOut}})   
  timeLineAbout.fromTo(".about-head", {scale:0},{scale:1});
  timeLineAbout.fromTo(".about-information", {scale:0},{scale:1});
  timeLineAbout.fromTo(".about-text", {x:400,opacity:0},{x:0,opacity:1},'<');
 
 //animation Education&Skills
- const timeLineEducation = gsap.timeline({scrollTrigger:".Education-Skills-head",defaults:{duration:1.5,ease: Back.easeInOut.config(1.7)}})
+ const timeLineEducation = gsap.timeline({scrollTrigger:".Education-Skills-head",defaults:{duration:0.8,ease: Back.easeInOut.config(1.7)}})
  timeLineEducation.fromTo(".Education-Skills-head", {scale:0 ,rotate:-45},{scale:1,rotate:360});
- timeLineEducation.fromTo(".Education-information", {scale:0,opacity:0 ,rotate:-45},{scale:1,opacity:1,rotate:360});
+ timeLineEducation.fromTo(".Education-information", {scale:0,opacity:0},{scale:1,opacity:1});
  timeLineEducation.fromTo(".Skill-Cards", {y:400,opacity:0},{y:0,opacity:1},'<');
  timeLineEducation.fromTo(".line", {opacity:0 ,width:"0px"},{opacity:1 ,width:"100%"});
 
  //animation What i do?
 
- const timeLineService = gsap.timeline({scrollTrigger:".box-part",defaults:{duration:0.5, ease: Power0.easeNone}})
- timeLineService.fromTo(".box-part1", {y:600 ,opacity:0},{y:0 ,opacity:1});
+ const timeLineService = gsap.timeline({scrollTrigger:".box-part",defaults:{duration:0.3, ease: Power0.easeNone}})
+ timeLineService.fromTo(".box-part1", {x:600 ,opacity:0},{x:0 ,opacity:1});
  timeLineService.fromTo(".box-part2", {y:600 ,opacity:0},{y:0 ,opacity:1});
- timeLineService.fromTo(".box-part3", {y:600 ,opacity:0},{y:0 ,opacity:1});
+ timeLineService.fromTo(".box-part3", {x:600 ,opacity:0},{x:0 ,opacity:1});
 
   //animation Portfolio
-  const timeLinePortfolio = gsap.timeline({scrollTrigger:"#Portfolio",defaults:{duration:4}})
+  const timeLinePortfolio = gsap.timeline({scrollTrigger:"#Portfolio",defaults:{duration:1}})
   timeLinePortfolio.fromTo('#Portfolio',{opacity:0},{opacity:1})
 
 // create a pop up for showing portfolio images
@@ -140,22 +156,38 @@ const projectsData = [
 
 
 ]
-
+// show preview project
 showImgProjects.forEach(element => {
-    element.addEventListener("click",()=>{
+    element.addEventListener("click",(e)=>{
+
         popup.classList.add("active")
+
         popupImg.classList.add("active")
+
         const currentItem = element.getAttribute("data-number");
         imgSrc[0].src = projectsData[currentItem - 1].projectImg1
         imgSrc[1].src = projectsData[currentItem - 1].projectImg2
 
     })
 });
+
+
+// close preview project
+popup.addEventListener("click",(e)=>{
+
+    if (e.target.classList.contains("portfolio-popup")) {
+        popup.classList.remove("active")
+    }
+
+})
+
 closePopUp.addEventListener("click",()=>{
     popup.classList.remove("active")
 })
 
+
 // loading page
+
 const spinner = document.querySelector(".spinner")
 window.addEventListener("load",()=>{
     spinner.classList.add("active")
